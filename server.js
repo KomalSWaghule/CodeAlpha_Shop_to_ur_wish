f5061093494d5c77d64fbdce558d8ef03be1dc77
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-11 21:34:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config(); // Load environment variables from .env
+ // Load environment variables from .env
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
