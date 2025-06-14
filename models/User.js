8140a4d042d72ca6910c5d91e2bd69049e69397e
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-14 13:52:54 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,7 +6,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'seller', 'admin'], default: 'user' }
-});
+}
+          );
 
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
@@ -14,10 +15,12 @@ UserSchema.pre('save', async function (next) {
   try {
     this.password = await bcrypt.hash(this.password, 10);
     next();
-  } catch (err) {
+  }
+  catch (err) {
     next(err);
   }
-});
+} 
+   );
 
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
